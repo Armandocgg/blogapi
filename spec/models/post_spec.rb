4cb31d0583d4712ddb918546b8b585332de37419
Armando Gúzman <armando@ideamosweb.com>
2018-12-22 22:38:48 -0500

    Models.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+  describe "Validations" do
+    it "Validate presence of required fields" do
+      should validate_presence_of(:title)
+      should validate_presence_of(:content)
+      should validate_presence_of(:published)
+      should validate_presence_of(:user_id)
+    end
+    
+  end
+
+end
